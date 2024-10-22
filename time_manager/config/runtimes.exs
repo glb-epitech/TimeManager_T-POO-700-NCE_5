3b7a582b670efe7955fa72a853ed814a7736e927
glb-epitech <gael.le-breton@epitech.eu>
2024-10-22 16:47:39 +0200

    Quick boot runtimes

--- time_manager/config/runtimes.exs
+++ time_manager/config/runtimes.exs
@@ -1,19 +1,14 @@
-# backend/config/runtime.exs
+# config/runtime.exs
 import Config
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
   config :time_manager, TimeManager.Repo,
-    url: database_url,
+    adapter: Ecto.Adapters.Postgres,  # Explicitly set adapter
+    url: System.get_env("DATABASE_URL"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     ssl: [verify: :verify_none]
 
+  # The secret key base is used to sign/encrypt cookies and other secrets
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -21,14 +16,23 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :time_manager, TimeManagerWeb.Endpoint,
     server: true,
-    url: [scheme: "https", host: System.get_env("PHX_HOST"), port: 443],
+    url: [host: host, port: port],
     http: [
       ip: {0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
+
+  # Configure your database
+  config :time_manager,
+    ecto_repos: [TimeManager.Repo]
+
+  # Force IPv4 to speed up startup
+  config :time_manager, TimeManager.Repo,
+    socket_options: [:inet6]
 end
