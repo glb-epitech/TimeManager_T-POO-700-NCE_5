11dae0befd1e3e5d54d7770951da4d9f020c00a7
glb-epitech <gael.le-breton@epitech.eu>
2024-10-22 11:19:55 +0200

    Config backend added in config folder

--- config/prod.exs
+++
@@ -1,8 +0,0 @@
-# config/prod.exs
-import Config
-
-config :time_manager, TimeManagerWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
-# Do not print debug messages in production
-config :logger, level: :info
